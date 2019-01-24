75a07d1ed51de251b0e52b8509ee6a68617cf8e2
Dani-Briggs <42754006+Dani-Briggs@users.noreply.github.com>
2019-01-24 15:49:48 -0700

    started script

--- Hw-2/script.js
+++ Hw-2/script.js
@@ -1,5 +1,6 @@
 /*
 Author: Dani Briggs
 Date: Jan 24, 2019
-Purpose: display a console log with my favorite  foods
+Purpose: display a console log with my favorite things involing food
 */
+console.log("Top 3 Restaurants: Pizza Hut, Paradise Falls, Montana Club. Top 3 Snacks: Hershy Bars, Popcorn, Oreo Cookies. Top 3 Fruits: Bananas, Grapes, Apples ");
