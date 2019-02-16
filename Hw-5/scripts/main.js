4bbbbe27152850fd00dbcc7cb84287aff54ec9ad
Dani-Briggs <42754006+Dani-Briggs@users.noreply.github.com>
2019-02-16 12:06:01 -0700

    did index and css

--- Hw-5/scripts/main.js
+++ Hw-5/scripts/main.js
@@ -3,3 +3,6 @@ Author: Dani Briggs
 Date: Feb 16, 2019
 Purpose: matching game
 */
+
+//create array for imageTags
+var imageTags = ["image1", "image2","image3", "image4", "image5", "image6", "image7", "image8", "image9","image10"]
