00e3512a0dfc1db5f7377551ec7f740b79f44d9d
Dani-Briggs <42754006+Dani-Briggs@users.noreply.github.com>
2019-03-04 15:32:59 -0700

    test

---
+++ Hw-7/scripts/main.js
@@ -0,0 +1,29 @@
+/*
+Author: Dani Briggs
+Date: March 4, 2019
+Purpose: Viewmaster
+*/
+
+
+class ViewClass
+{
+    constructor(title)
+    {
+      this.title = title;
+
+    }
+
+    get theTitle()
+    {
+       return this.title;
+
+    }
+
+
+}
+
+function accessInfo()
+{
+ var myViewFinder = new ViewFinder("pug");
+
+}
--- Hw-7/scripts/script.js
+++
@@ -1,5 +0,0 @@
-/*
-Author: Dani Briggs
-Date: Feb 1, 2019
-Purpose: chose your own adveture story
-*/
