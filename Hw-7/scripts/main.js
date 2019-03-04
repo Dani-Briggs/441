9ed9c802b87588e13f274f8920e446a11e9e9bd2
Dani-Briggs <42754006+Dani-Briggs@users.noreply.github.com>
2019-03-04 16:30:25 -0700

    test1

--- Hw-7/scripts/main.js
+++ Hw-7/scripts/main.js
@@ -7,14 +7,23 @@ var myViewFinderArray = new Array();
 
 class ViewClass
 {
-    constructor(title)
+    constructor(title, author, year, description, image)
     {
       this.title = title;
+      this.author = author;
+      this.year = year;
+      this.description = description;
+      this.image = image;
+
 
     }
     toString()
     {
       return "Title: " + this.title;
+      return "Author: " + this.author;
+      return "Year: " + this.year;
+      return "Description: " + this.description;
+      return this.image;
     }
 
     get theTitle()
@@ -24,19 +33,26 @@ class ViewClass
     }
     get imageAuthor()
     {
-
+        return this.author;
     }
 
-  get imageYear()
+    get imageYear()
     {
-
+        return this.year;
     }
-
+    get imageDes()
+    {
+        return this.description;
+    }
+    get theImage()
+    {
+        return this.image;
+    }
 
 }
 function initialArray()
 {
-  var myViewFinder = new ViewFinder("vegas");
+  var myViewFinder = new ViewFinder("Muse in Vegas");
   myViewFinderArray.push(myViewFinder);
 
 }
