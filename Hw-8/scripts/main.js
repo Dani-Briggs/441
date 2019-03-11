4b200430ea3247d431ce6bb79f89bcc950deaa30
Dani-Briggs <42754006+Dani-Briggs@users.noreply.github.com>
2019-03-11 16:47:57 -0600

    test

--- Hw-8/scripts/main.js
+++ Hw-8/scripts/main.js
@@ -9,6 +9,7 @@ $(document).ready(function(){
   $("button").click(function(){
    setInterval(moveSquare(), 1000);
     $("#port").fadeOut().fadeIn()
+    $("#textone").fadeOut()
   });
 
 
@@ -16,5 +17,5 @@ $(document).ready(function(){
 
 function moveSquare(){
     $("#square").animate({left:'250px'}).animate({top:'450px'}).animate({left:'0px'}).animate({top:'100px'}).fadeOut()
-  
+
 }
