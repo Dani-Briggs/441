b452b46370ad160268c310c7c8501fa954c2399a
Dani-Briggs <42754006+Dani-Briggs@users.noreply.github.com>
2019-03-11 15:59:49 -0600

    test

--- Hw-8/scripts/main.js
+++ Hw-8/scripts/main.js
@@ -3,6 +3,11 @@ Author: Dani Briggs
 Date: March 11, 2019
 Purpose: jquery
 */
-/*$(document).ready(function(){
+$(document).ready(function(){
 
-});*/
+  $("button").click(function(){
+    $("#square").animate({left:'250px'})
+  });
+
+
+});
