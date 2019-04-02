9403f58ade4a05848c114a9cb27dbc0d3f67adab
Dani-Briggs <42754006+Dani-Briggs@users.noreply.github.com>
2019-04-02 16:05:34 -0600

    fix

--- Hw-9/scripts/main.js
+++ Hw-9/scripts/main.js
@@ -15,20 +15,20 @@ $(document).ready(function(){
                 + poke.num + "<br>Name:"
                 + poke.name + "<br>Picture:"
                 + poke.img + "<br>Type:"
-                + poke.type +"<br>Height:"
-                + poke.height +"<br>Weight:"
-                + poke.weight +"<br>Candy:"
-                + poke.candy +"<br>Candy Count:"
-                + poke.candy_count +"<br>Egg:"
-                + poke.egg +"<br>Spawn Chance:"
-                + poke.spawn_chance +"<br>Average Spawn:"
-                + poke.avg_spawns +"<br>Spawn Time:"
-                + poke.spawn_time +"<br>Multipliers:"
+                + poke.type + "<br>Height:"
+                + poke.height + "<br>Weight:"
+                + poke.weight + "<br>Candy:"
+                + poke.candy + "<br>Candy Count:"
+                + poke.candy_count + "<br>Egg:"
+                + poke.egg + "<br>Spawn Chance:"
+                + poke.spawn_chance + "<br>Average Spawn:"
+                + poke.avg_spawns + "<br>Spawn Time:"
+                + poke.spawn_time + "<br>Multipliers:"
                 + poke.multipliers + "<br>Weaknesses:<br>" +
                 poke.weaknesses[0] + "<br>" + poke.weaknesses[1] + "<br>" + poke.weaknesses[2] + "<br>" + poke.weaknesses[3]
-                "<br>Evolves Into:<br>" +
+                + "<br>Evolves Into:<br>" +
                 poke.next_evolution.num + "<br>" + poke.next_evolution.name
-                "<br>Pervous Evolution:<br>" +
+                + "<br>Pervous Evolution:<br>" +
                 poke.prev_evolution.num + "<br>" + poke.prev_evolution.name
         );
       });
