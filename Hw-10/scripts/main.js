f9cfc80f571b0269d20be3aa3980d96e8df30a40
Dani-Briggs <42754006+Dani-Briggs@users.noreply.github.com>
2019-04-09 16:14:27 -0600

    test basic

--- Hw-10/scripts/main.js
+++ Hw-10/scripts/main.js
@@ -1,5 +1,9 @@
 /*
 Author: Dani Briggs
 Date: April 8, 2019
-Purpose: jquery
+Purpose: HTML5
 */
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+ctx.fillStyle = "#0000FF";
+ctx.fillRect(50, 50, 10, 10);
