7f168730e3e987093ef5055130827d59a666f837
Dani-Briggs <42754006+Dani-Briggs@users.noreply.github.com>
2019-04-09 16:36:12 -0600

    added keys

--- Hw-10/scripts/main.js
+++ Hw-10/scripts/main.js
@@ -13,3 +13,38 @@ function drawSquare()
         ctx.clearRect(0,0,800,600);
         ctx.fillRect(x, y, 20, 20);
     }
+$(document).ready(function(){
+  $(this).keypress(function(event){
+    getKey(event);
+  })
+})
+
+function getKey(event){
+  var char = event.which || event.keyCode;
+  var actualLetter = String.fromCharCode(char);
+  if(actualLetter == "w"){
+    moveUp();
+  }
+  if(actualLetter == "s"){
+    moveDown();
+  }
+  if(actualLetter == "d"){
+    moveRight();
+  }
+  if(actualLetter == "a"){
+    moveLeft();
+  }
+  drawSquare();
+}
+function moveUp(){
+  y-=10
+}
+function moveDown(){
+  y+=10
+}
+function moveRight(){
+  x-=10
+}
+function moveLeft(){
+  x+=10
+}
